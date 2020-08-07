ffc668cba116062dc487e35ea771d8dbffa4fcd7
Chikara Takahashi <chtakahashi10@gmail.com>
2020-08-07 12:43:19 -0400

    pulled pwd out to separte js file

--- bash.js
+++ bash.js
@@ -2,11 +2,12 @@ process.stdout.write('prompt > ');
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  if (cmd === "pwd"){
-    //process.stdout.write(__dirname)
-    console.log(process.cwd())
+  if (cmd === "pwd") {
+    const pwdObject = require('./pwd')
+    pwdObject.pwd()
   }
   //process.stdout.write("You typed: " + cmd);
   process.stdout.write("\nprompt > ");
 
 })
+
